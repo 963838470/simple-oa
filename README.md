f9503fce76532cf555223b0530dfd7518eedd6b9
1 <1>
2018-01-05 11:50:27 +0800

    新增ef

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 asp.net 4.6.1
 webapi
 SwaggerUI - webapi文档注释
+Entity Framework 6.x
--- oa_server/oa_server/Controllers/UserController.cs
+++ oa_server/oa_server/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using oa_server.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -12,12 +13,7 @@ namespace oa_server.Controllers
     /// </summary>
     public class UserController : ApiController
     {
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-        };
+        private OAEntities db = new Models.OAEntities();
 
         /// <summary>
         /// 获取所有用户
@@ -25,7 +21,7 @@ namespace oa_server.Controllers
         /// <returns></returns>
         public IHttpActionResult Get()
         {
-            return Json(products);
+            return Json(db.AuthorityUser.ToList());
         }
 
         /// <summary>
@@ -35,16 +31,16 @@ namespace oa_server.Controllers
         /// <returns></returns>
         public IHttpActionResult Get(int id)
         {
-            var product = products.FirstOrDefault((p) => p.Id == id);
+            var product = db.AuthorityUser.FirstOrDefault(o => o.Id == id);
             return Json(product);
         }
-    }
 
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public decimal Price { get; set; }
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]AuthorityUser user)
+        {
+            user = db.AuthorityUser.Add(user);
+            db.SaveChanges();
+            return Json(user);
+        }
     }
 }
