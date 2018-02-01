756981e062f23d68a123e8e988bc65cfa5c3ef97
r720 <963838470@qq.com>
2018-02-01 22:41:47 +0800

    删除无用页面

--- ui/src/route.js
+++ ui/src/route.js
@@ -18,8 +18,6 @@ export default new Router({
       iconCls: 'el-icon-message',
       children: [
         { path: '/ouuser', name: '机构人员', component: resolve => require(['./components/base/ouUser.vue'], resolve) },
-        { path: '/company', name: '公司管理', component: resolve => require(['./components/base/company.vue'], resolve) },
-        { path: '/department', name: '部门管理', component: resolve => require(['./components/base/department.vue'], resolve) },
         { path: '/user', name: '人员管理', component: resolve => require(['./components/base/user.vue'], resolve) },
         { path: '/project', name: '项目管理', component: resolve => require(['./components/base/project.vue'], resolve) },
         { path: '/default', name: '首页', component: resolve => require(['./components/base/default.vue'], resolve), alias: '/', hidden: true },
