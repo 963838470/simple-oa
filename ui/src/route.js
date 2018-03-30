87ddfa64c0eca465853b41321d1db0214a457a56
963838470 <963838470@qq.com>
2018-03-30 18:03:24 +0800

    增加格式化配置

--- ui/src/route.js
+++ ui/src/route.js
@@ -1,30 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from './components/Login.vue'
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./components/Login.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/login',
+      path: "/login",
       component: Login,
       hidden: true
     },
     {
-      path: '/',
-      name: '基础管理',
-      alias: '/default',
+      path: "/",
+      name: "基础管理",
+      alias: "/default",
       component: resolve => require(["./components/HomePage.vue"], resolve),
-      iconCls: 'el-icon-message',
+      iconCls: "el-icon-message",
       children: [
-        { path: '/ouuser', name: '机构人员', component: resolve => require(['./components/base/OuUser.vue'], resolve) },
-        { path: '/project', name: '项目管理', component: resolve => require(['./components/base/Project.vue'], resolve) },
-        { path: '/default', name: '首页', component: resolve => require(['./components/base/Default.vue'], resolve), alias: '/', hidden: true },
+        { path: "/ouuser", name: "机构人员", component: resolve => require(["./components/base/OuUser.vue"], resolve) },
+        { path: "/project", name: "项目管理", component: resolve => require(["./components/base/Project.vue"], resolve) },
+        { path: "/default", name: "首页", component: resolve => require(["./components/base/Default.vue"], resolve), alias: "/", hidden: true }
       ]
     },
     {
-      path: '*',
+      path: "*",
       component: resolve => require(["./components/404.vue"], resolve),
       hidden: true
     }
